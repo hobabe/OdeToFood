2fb069737b44c1f7713401f1e74ee171b4793daf
Tuan Ngo <cjhobabe@gmail.com>
2023-08-10 17:38:59 +0700

    Refactor: moving code from Order into Restaurant

--- OdeToFood/Pages/Order/List.cshtml.cs
+++ OdeToFood/Pages/Order/List.cshtml.cs
@@ -1,27 +1,11 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Configuration;
-using OdeToFood.Data;
 
 namespace OdeToFood.Pages.Order
 {
     public class ListModel : PageModel
     {
-        private readonly IConfiguration config;
-        private readonly IRestaurantData restaurantData;
-
-        public string Message { get; set; }
-        public string MessageFromAppsettings { get; set; }
-
-        public ListModel(IConfiguration config, IRestaurantData restaurantData)
-        {
-            this.config = config;
-            this.restaurantData = restaurantData;
-        }
         public void OnGet()
         {
-            Message = "Hello, world!";
-            MessageFromAppsettings = config["Message"];
         }
     }
 }
