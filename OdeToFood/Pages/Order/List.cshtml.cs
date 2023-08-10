4b2b55c8361bb3f22a262f3066f338f861618ccc
Tuan Ngo <cjhobabe@gmail.com>
2023-08-10 17:37:11 +0700

    S7 Registering a Data Service

--- OdeToFood/Pages/Order/List.cshtml.cs
+++ OdeToFood/Pages/Order/List.cshtml.cs
@@ -1,19 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using OdeToFood.Data;
 
 namespace OdeToFood.Pages.Order
 {
     public class ListModel : PageModel
     {
         private readonly IConfiguration config;
+        private readonly IRestaurantData restaurantData;
 
         public string Message { get; set; }
         public string MessageFromAppsettings { get; set; }
 
-        public ListModel(IConfiguration config)
+        public ListModel(IConfiguration config, IRestaurantData restaurantData)
         {
             this.config = config;
+            this.restaurantData = restaurantData;
         }
         public void OnGet()
         {
