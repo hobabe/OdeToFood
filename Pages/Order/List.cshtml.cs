ade3338df78df50594ba3d5665776115ea2be98f
Tuan Ngo <cjhobabe@gmail.com>
2023-08-10 17:04:34 +0700

    S4 - Injecting and Using Configuration
    quick:
    ctor - create new contrutor
    alt+ enter at var create new Variable set read only and assign it with parameter

--- Pages/Order/List.cshtml.cs
+++ Pages/Order/List.cshtml.cs
@@ -1,12 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace OdeToFood.Pages.Order
 {
     public class ListModel : PageModel
     {
+        private readonly IConfiguration config;
+
+        public string Message { get; set; }
+        public string MessageFromAppsettings { get; set; }
+
+        public ListModel(IConfiguration config)
+        {
+            this.config = config;
+        }
         public void OnGet()
         {
+            Message = "Hello, world!";
+            MessageFromAppsettings = config["Message"];
         }
     }
 }
