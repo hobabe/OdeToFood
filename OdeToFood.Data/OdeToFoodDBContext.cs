31b2f02a52fe0bfc23b78e56e8c7e76c762cab27
Tuan Ngo <cjhobabe@gmail.com>
2023-08-14 17:35:23 +0700

    S2 Implementing an Entity Framework DbContext

---
+++ OdeToFood.Data/OdeToFoodDBContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OdeToFood.Core;
+
+namespace OdeToFood.Data
+{
+    public class OdeToFoodDBContext :DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
